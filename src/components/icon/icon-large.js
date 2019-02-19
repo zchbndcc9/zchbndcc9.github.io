04881560ca98327cc6ad0ceff7579acde809392c
Zach Banducci <zbducci@gmail.com>
2019-02-19 12:51:42 -0600

    Added icon components that will show language icon

---
+++ src/components/icon/icon-large.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Icon from './icon';
+
+const IconLarge = ({ ...props }) => (
+  <Icon style={{ width: "15em", padding: "2.5em"}} {...props} />
+)
+
+export default IconLarge
